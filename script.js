18239f22cf2a4ddcaab3e2795ccd45ca1d6af0c4
KellynMoodley <KMoodley@datacentrix.co.za>
2025-02-19 09:16:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ async function fetchAccountData(accountNumber) {
 
 // Function to update the dashboard UI
 function updateDashboard(data) {
+    console.log('Received data:', data); // Add this line to debug
     // Update Call Strategy Section
     if (data[0]) {
         strategySection.innerHTML = `
